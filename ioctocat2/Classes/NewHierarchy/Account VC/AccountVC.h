4ec51dc3a38f8303162f180fe888447cbbbdad5b
limbo0312 <limbo0312@foxmail.com>
2014-07-25 18:52:21 +0800

    update

---
+++ ioctocat2/Classes/NewHierarchy/Account VC/AccountVC.h
@@ -0,0 +1,13 @@
+//
+//  AccountVC.h
+//  iOctocat
+//
+//  Created by EGS on 14-7-25.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountVC : UIViewController
+
+@end
---
+++ ioctocat2/Classes/NewHierarchy/Account VC/AccountVC.m
@@ -0,0 +1,52 @@
+//
+//  AccountVC.m
+//  iOctocat
+//
+//  Created by EGS on 14-7-25.
+//
+//
+
+#import "AccountVC.h"
+
+@interface AccountVC ()
+
+@end
+
+@implementation AccountVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [self.navigationController setNavigationBarHidden:YES];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
