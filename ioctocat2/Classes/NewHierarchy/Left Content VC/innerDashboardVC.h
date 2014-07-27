67d676a69b66d7f131513ef3372b8c30a09cbcab
Engels <limbo0312@gmail.com>
2014-07-27 23:20:41 +0800

    innerDashboard

---
+++ ioctocat2/Classes/NewHierarchy/Left Content VC/innerDashboardVC.h
@@ -0,0 +1,13 @@
+//
+//  innerDashboardVC.h
+//  iOctocat
+//
+//  Created by Engels on 14-7-27.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface innerDashboardVC : UIViewController
+
+@end
---
+++ ioctocat2/Classes/NewHierarchy/Left Content VC/innerDashboardVC.m
@@ -0,0 +1,39 @@
+//
+//  innerDashboardVC.m
+//  iOctocat
+//
+//  Created by Engels on 14-7-27.
+//
+//
+
+#import "innerDashboardVC.h"
+
+@interface innerDashboardVC ()
+
+@end
+
+@implementation innerDashboardVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
